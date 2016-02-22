b2f83030552aeca48b4ee3c77cce911b286c227f
katherine.a.oliver.16@dartmouth.edu <katherine.a.oliver.16@dartmouth.edu>
2016-02-22 17:27:27 -0500

    incompletecount

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -4,11 +4,22 @@ if (Meteor.isClient) {
    // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      // Show newest tasks at the top
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+   },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
-  
+ 
   Template.body.events({
     "submit .new-task": function (event) {
       // Prevent default browser form submit
@@ -24,7 +35,10 @@ if (Meteor.isClient) {
       });
  
       // Clear form
-         event.target.text.value = "";
+        event.target.text.value = "";
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
  
