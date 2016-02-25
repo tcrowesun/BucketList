0caea704af8b2ba74db3321d4bcf5c03c0d35075
ntejada <nict25@gmail.com>
2016-02-25 15:03:47 -0500

    task

--- simple-todos/router.js
+++ simple-todos/router.js
@@ -13,13 +13,15 @@ Router.route('/taskPage/:_id', function() {
   this.render('taskPage', {data: function(){
     return Tasks.findOne({_id: this.params._id});
   }});
+});
 
 Router.route('/categoryList/:_category', {
   path: '/categoryList/:_category',
   template: 'categoryList',
   data: function () {
     return {
-      categoryTasks: Tasks.find({category: this.params._category}, {sort: {createdAt: -1}})
+      categoryTasks: Tasks.find({category: this.params._category}, {sort: {createdAt: -1}}),
+      category: this.params._category
     }
   }
 });
