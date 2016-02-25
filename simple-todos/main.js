36923b4720143480dfbfd8d9b7dc0bd9c522e07b
katherine.a.oliver.16@dartmouth.edu <katherine.a.oliver.16@dartmouth.edu>
2016-02-24 20:38:03 -0500

    functional completed page, move completed items from main page to completed page

--- simple-todos/main.js
+++ simple-todos/main.js
@@ -6,17 +6,12 @@ if (Meteor.isClient) {
    // This code only runs on the client
   Template.simpleTodos.helpers({
     tasks: function () {
-      if (Session.get("hideCompleted")) {
-        // If hide completed is checked, filter tasks
+
         return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-      } else {
-        // Otherwise, return all of the tasks
-        return Tasks.find({}, {sort: {createdAt: -1}});
-      }
+  
+       
    },
-    hideCompleted: function () {
-      return Session.get("hideCompleted");
-    },
+  
     incompleteCount: function () {
       return Tasks.find({checked: {$ne: true}}).count();
     }
@@ -39,9 +34,7 @@ if (Meteor.isClient) {
       // Clear form
         event.target.text.value = "";
     },
-    "change .hide-completed input": function (event) {
-      Session.set("hideCompleted", event.target.checked);
-    }
+    
   });
 
   Template.task.events({
@@ -56,6 +49,8 @@ if (Meteor.isClient) {
     }
   });
 
+
+
   Template.navigation.events({
     "click .hamburger-menu": function(){
       if(isTrue){
@@ -68,5 +63,14 @@ if (Meteor.isClient) {
         isTrue = true;
       }
     }
+  });
+
+  Template.past.helpers({
+      completedTasks: function () {
+      
+        return Tasks.find({checked: {$ne: false}}, {sort: {createdAt: -1}});
+      
+   },
+
   });
 }
